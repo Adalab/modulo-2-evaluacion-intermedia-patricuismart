ccf844f2d814f5182c77dfc9dd7a5d3a7de96b96
patricuismart <patricuismart@gmail.com>
2021-09-01 09:33:50 +0200

    Declare variables, randon function and eventlistener

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 'use strict';
-console.log('hello world');
+
 //Declaración de variables
+const userNumber = document.querySelector('.js_number');
+const tryButon = document.querySelector('.js_buton');
+const userClue = document.querySelector('.js_clue');
+const counter = document.querySelector('.js_counter');
 
 //Funciones
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+const number = getRandomNumber(100);
+console.log(number);
 
+function hadleClickTryButon() {
+  console.log('has hecho click');
+}
 //Eventos
+tryButon.addEventListener('click', hadleClickTryButon);
